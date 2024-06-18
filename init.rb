fa4e873b9968a56985355b9e033dc7118d7c245a
baltasarromero <baltasarromero@gmail.com>
2024-06-18 12:49:05 -0300

    used init code from production. Only the authors changed

--- init.rb
+++ init.rb
@@ -1,10 +1,10 @@
 Redmine::Plugin.register :report do
   name 'Billing report plugin'
-  author 'Maximiliano A. Sariego'
+  author 'Leandro Perez Torres & German Vicentin'
   # description 'This is a plugin for Redmine'
   version '0.0.1'
   # url 'http://example.com/path/to/plugin'
   # author_url 'http://example.com/about'
-  
+
   menu :top_menu, :global_activity, { :controller => 'report', :action => 'index', :year => Time.now.year }, :last => true, :caption => "Facturación", :if => Proc.new { User.current.admin? }
 end
