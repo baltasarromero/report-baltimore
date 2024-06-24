9d401497df6f96ba54e6be8dea15fd3e1ff36fc6
baltasarromero <baltasarromero@gmail.com>
2024-06-24 14:51:29 -0300

    remove unnecessary returns

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -17,72 +17,53 @@ class ReportController < ApplicationController
   private
 
   def fetch_billing_types(projects)
+    return [] if projects.empty?
+
     billing_types = []
-    elapsed_time = Benchmark.realtime do
-      billing_type = CustomField.find_by(name: 'Tipo de Facturacion')
-      return [] unless billing_type
-    
-      billing_types = projects.first.available_custom_fields
-                                .select { |cf| cf.name == billing_type.name }
-                                .first&.possible_values || []
-    
-      billing_types.push("") unless billing_types.include?("")
-    end
-    logger.info("Elapsed time getting billing types: #{elapsed_time} seconds")   
-    billing_types
+    billing_type_field = CustomField.find_by(name: 'Tipo de Facturacion')
+    return [] unless billing_type_field
+
+    billing_types = projects.first.available_custom_fields
+                            .select { |cf| cf.name == billing_type_field.name }
+                            .first&.possible_values || []
+
+    billing_types.push("") unless billing_types.include?("")
   end
 
   def group_projects_by_billing_type(projects)
     projects_by_billing_type = {}
-    elapsed_time = Benchmark.realtime do
-      projects_by_billing_type = projects.each_with_object({}) do |project, hash|
-        hash[project.billing_type] ||= []
-        insertion_index = hash[project.billing_type].bsearch_index { |p| p.name > project.name } || hash[project.billing_type].size
-        hash[project.billing_type].insert(insertion_index, project)
-      end
+    projects_by_billing_type = projects.each_with_object({}) do |project, hash|
+      hash[project.billing_type] ||= []
+      insertion_index = hash[project.billing_type].bsearch_index { |p| p.name > project.name } || hash[project.billing_type].size
+      hash[project.billing_type].insert(insertion_index, project)
     end
-    logger.info("Elapsed time grouping and ordering projects: #{elapsed_time} seconds")
-    logger.info("these are the grouped and sorted projects: #{projects_by_billing_type} seconds")
 
-    projects_by_billing_type
-  end
-  
-
+  end  
 
   def get_hours_by_project_month(year, non_billable_entry_id, billable_projects_ids)
     time_entries_dict = {}
-    elapsed_time = Benchmark.realtime do
-      time_entries = TimeEntry.select('project_id, tmonth AS month, SUM(hours) AS total_monthly_hours').where(tyear: year).where.not(activity_id: non_billable_entry_id).where(project_id: billable_projects_ids).group(:project_id, :tmonth).order(:project_id, :tmonth)
-      
-      # Convert to a dictionary of dictionaries
-      time_entries_dict = time_entries.each_with_object({}) do |entry, hash|
-        hash[entry.project_id] ||= {}
-        hash[entry.project_id][entry.month] = entry.total_monthly_hours
-      end
-    end  
-    logger.info("Elapsed time getting time entries: #{elapsed_time} seconds") 
-    time_entries_dict
+    time_entries = TimeEntry.select('project_id, tmonth AS month, SUM(hours) AS total_monthly_hours').where(tyear: year).where.not(activity_id: non_billable_entry_id).where(project_id: billable_projects_ids).group(:project_id, :tmonth).order(:project_id, :tmonth)
+
+    # Convert to a dictionary of dictionaries
+    time_entries_dict = time_entries.each_with_object({}) do |entry, hash|
+      hash[entry.project_id] ||= {}
+      hash[entry.project_id][entry.month] = entry.total_monthly_hours
+    end
   end  
 
   def get_invoiceable_projects()
     projects = []
-    elapsed_time = Benchmark.realtime do
-      # Get the id of the custom field needed
-      hide_project_custom_field = CustomField.find_by(name: 'Ocultar en Facturacion')
-      # Get the id of the custom field needed
-      billing_type = CustomField.find_by(name: 'Tipo de Facturacion')
-      # A value of 0 for the custom field indicates that the project is NOT hidden for invoiving reports   
-      projects = Project
-      .joins("INNER JOIN custom_values cvof ON cvof.customized_id = projects.id AND cvof.customized_type = 'Project' AND cvof.custom_field_id = #{hide_project_custom_field.id} AND cvof.value = 0")
-      .joins("INNER JOIN custom_values cvbt ON cvbt.customized_id = projects.id AND cvbt.customized_type = 'Project' AND cvbt.custom_field_id = #{billing_type.id}")
-      .joins("LEFT JOIN enabled_modules em ON em.project_id = projects.id AND em.name = 'proformanext'")
-      .select("projects.id, projects.name, projects.identifier, cvbt.value AS billing_type, 
-          CASE WHEN em.id IS NOT NULL THEN TRUE ELSE FALSE END AS proformanext_enabled")
-          .order("billing_type ASC")
-    end
-    logger.info("Elapsed time getting invoiceable projects: #{elapsed_time} seconds") 
-    
-    #rendered_projects = render json: projects
-    projects
+    # Get the id of the custom field needed
+    hide_project_custom_field = CustomField.find_by(name: 'Ocultar en Facturacion')
+    # Get the id of the custom field needed
+    billing_type = CustomField.find_by(name: 'Tipo de Facturacion')
+    # A value of 0 for the custom field indicates that the project is NOT hidden for invoiving reports   
+    projects = Project
+    .joins("INNER JOIN custom_values cvof ON cvof.customized_id = projects.id AND cvof.customized_type = 'Project' AND cvof.custom_field_id = #{hide_project_custom_field.id} AND cvof.value = 0")
+    .joins("INNER JOIN custom_values cvbt ON cvbt.customized_id = projects.id AND cvbt.customized_type = 'Project' AND cvbt.custom_field_id = #{billing_type.id}")
+    .joins("LEFT JOIN enabled_modules em ON em.project_id = projects.id AND em.name = 'proformanext'")
+    .select("projects.id, projects.name, projects.identifier, cvbt.value AS billing_type, 
+        CASE WHEN em.id IS NOT NULL THEN TRUE ELSE FALSE END AS proformanext_enabled")
+        .order("billing_type ASC")
   end  
 end
